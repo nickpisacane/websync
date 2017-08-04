219fe414aa82ec60aa9e1485f4f6efa0f3b366cb
Nick Pisacane <pisacanen@gmail.com>
2017-08-04 16:35:01 -0700

    Implement read on S3Item

--- src/S3Item.ts
+++ src/S3Item.ts
@@ -1,14 +1,20 @@
 import { S3 } from 'aws-sdk'
-import Item from './Item'
+import { Item } from './types'
+
+const s3 = new S3()
 
 export default class S3Item implements Item {
+  private s3Object: S3.Object
+  private bucketName: string
+
   public key: string
   public modtime: Date
   public size: number
   public isSymbolicLink: boolean
-  private s3Object: S3.Object
+  
 
-  constructor(s3Object: S3.Object) {
+  constructor(bucketName: string, s3Object: S3.Object) {
+    this.bucketName = bucketName
     this.s3Object = s3Object
     if (!s3Object.Key) {
       throw new Error(`S3Item: Key is required on s3Object`)
@@ -26,6 +32,25 @@ export default class S3Item implements Item {
   }
 
   private async getBody(): Promise<Buffer> {
+    const objectOutput = await s3.getObject({
+      Bucket: this.bucketName,
+      Key: this.s3Object.Key as string,
+    }).promise()
 
+    let ret: Buffer
+    const body = objectOutput.Body
+    if (typeof body === 'string') {
+      ret = new Buffer(body)
+    } else if (body instanceof Uint8Array) {
+      ret = Buffer.from(body.buffer)
+    } else {
+      ret = new Buffer(0)
+    }
+
+    return ret
+  }
+
+  public read(): Promise<Buffer> {
+    return this.getBody()
   }
 }
