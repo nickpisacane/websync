00a43606173cb355c4c602ece5328b7a720cc921
Nicholas Pisacane <pisacanen@gmail.com>
2017-08-05 23:28:23 -0700

    Store base directory for use when talking with fs

--- src/FileItem.ts
+++ src/FileItem.ts
@@ -1,13 +1,17 @@
+import * as Path from 'path'
 import * as fs from 'fs-extra'
 import { Item, Container } from './types'
 
 export default class FileItem implements Item {
+  private baseDirectory: string
+
   public key: string
   public modtime: Date
   public size: number
   public isSymbolicLink: boolean
 
-  constructor(fileName: string, stats: fs.Stats) {
+  constructor(baseDirectory: string, fileName: string, stats: fs.Stats) {
+    this.baseDirectory = baseDirectory
     this.key = fileName
     this.modtime = stats.mtime
     this.size = stats.size
@@ -15,11 +19,13 @@ export default class FileItem implements Item {
   }
 
   read(): Promise<Buffer> {
-    return fs.readFile(this.key)
+    return fs.readFile(Path.join(this.baseDirectory, this.key))
   }
 
-  public static async fromFileName(fileName: string): Promise<FileItem> {
-    const stats = await fs.stat(fileName)
-    return new FileItem(fileName, stats)
+  public static async fromFileName(baseDirectory: string, fileName: string): Promise<FileItem> {
+    const stats = await fs.stat(Path.join(baseDirectory, fileName))
+    fileName = fileName.replace(/^\//, '')
+
+    return new FileItem(baseDirectory, fileName, stats)
   }
 }
