efb6d4b595b0549b7ad4b3fac42931751d63c0a9
Nick Pisacane <pisacanen@gmail.com>
2017-10-02 19:26:51 -0700

    tests for GlobTable

---
+++ test/unit/GlobTable.test.ts
@@ -0,0 +1,13 @@
+import { expect } from 'chai'
+
+import GlobTable from '../../src/GlobTable'
+
+describe('GlobTable', () => {
+  it('basic lookup', () => {
+    const table = new GlobTable<string>({
+      './foo/**/*.js': 'bar',
+    })
+
+    expect(table.lookup('./foo/bar/bang.js')).to.equal('bar')
+  })
+})
