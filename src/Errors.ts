49995ad053887f044caff5a011ea2ea797f5db02
Nick Pisacane <pisacanen@gmail.com>
2017-10-02 19:19:45 -0700

    Rename and add errors for initialized/completed

---
+++ src/Errors.ts
@@ -0,0 +1,48 @@
+export const ErrorCodes = {
+  TransferFailed: 1,
+  TooManyInvalidations: 2,
+  InvalidationsFailed: 3,
+  AlreadyInitialized: 4,
+}
+
+export class Base extends Error {
+  public code: number
+
+  constructor(msg: string, code: number) {
+    super(msg)
+
+    this.code = code
+  }
+}
+
+export class TransferFailed extends Base {
+  public transferError: Error
+
+  constructor(transferError: Error) {
+    super(`Websync: Transfer Failed.`, ErrorCodes.TransferFailed)
+
+    this.transferError = transferError
+  }
+}
+
+export class TooManyInvalidations extends Base {
+  constructor() {
+    super(`Websync: Too many invalidations.`, ErrorCodes.TooManyInvalidations)
+  }
+}
+
+export class InvalidationsFailed extends Base {
+  public invalidationsError: Error
+
+  constructor(invalidationsError: Error) {
+    super(`Websync: Invalidations Failed.`, ErrorCodes.InvalidationsFailed)
+
+    this.invalidationsError = invalidationsError
+  }
+}
+
+export class AlreadyInitialized extends Base {
+  constructor() {
+    super(`Websync: Already initialized.`, ErrorCodes.AlreadyInitialized)
+  }
+}
