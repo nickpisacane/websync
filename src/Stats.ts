022f0e154dc0d650c1db73f6c3ad5977610c73bd
Nicholas Pisacane <pisacanen@gmail.com>
2018-01-06 10:58:16 -0800

    Add size property

--- src/Stats.ts
+++ src/Stats.ts
@@ -25,6 +25,7 @@ export default class Stats implements StatsObject {
   public completed: boolean
   public invalidated: boolean
   public time: number
+  public size: number
 
   constructor(stats: Partial<StatsObject> = {}) {
     this.update(stats)
